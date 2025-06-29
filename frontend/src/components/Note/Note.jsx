336f42d9d0cefe60cde5b8687c54f20bfec73675
BrunoMantovan <brunochupetin1@gmail.com>
2025-06-28 21:02:14 -0300

    finished 2.0

--- frontend/src/components/Note/Note.jsx
+++ frontend/src/components/Note/Note.jsx
@@ -66,6 +66,7 @@ export default function note(props) {
             color: color,
             isArchived: isArchived,
             position: positionRef.current,
+            categories: categories
         }
         console.log("Updating note with changes: ", noteChanges);
         
