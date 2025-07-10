18ed4f2c9f16f5b45f39c28536b407055e463680
BrunoMantovan <brunochupetin1@gmail.com>
2025-07-10 17:17:52 -0300

    centered header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,14 +1,14 @@
 
 export default function Header(props) {
   return (
-    <header style={{ display: "flex", alignItems: "center", gap: "1rem", margin: "0, 1rem", width: "100%", padding: "1rem"}}>
-        <h1 style={{textAlign:"center", margin: "auto"}}>Fullstack notes</h1>
+    <header style={{ position: "relative", display: "flex", alignItems: "center", width: "100%", padding: "1rem", minHeight: "3rem"}}>
+        <h1 style={{position: "absolute", left:"50%", top:"50%", transform:"translate(-50%, -50%)", width: "max-content", textAlign:"center", margin: 0}}>Fullstack notes</h1>
         <input
           type="text"
           placeholder="Filter by category..."
           value={props.categoryFilter}
           onChange={e => props.setCategoryFilter(e.target.value)}
-          style={{ padding: "0.5rem", borderRadius: "8px", border: "1px solid #ccc" }}
+          style={{ padding: "0.5rem", borderRadius: "8px", border: "1px solid #ccc", marginLeft: "auto", marginRight: "1rem", width: "200px" }}
         />
     </header>
   )
